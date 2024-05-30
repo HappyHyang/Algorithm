414b1645c1dec0944a09fab553e03a8700573af5
GyeongHyang Jun <48426244+HappyHyang@users.noreply.github.com>
2024-05-30 21:46:35 +0900

    [Bronze V] Title: A×B, Time: 80 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens = null;
+
+	public static void main(String[] args) throws Exception {
+		tokens = new StringTokenizer(input.readLine());
+		System.out.println(Integer.parseInt(tokens.nextToken()) * Integer.parseInt(tokens.nextToken()));
+	}
+}
