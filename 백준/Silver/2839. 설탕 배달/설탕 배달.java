96c8a3d0fe542284863ffe1b988c38f122095474
GyeongHyang Jun <48426244+HappyHyang@users.noreply.github.com>
2024-06-07 17:41:44 +0900

    [Silver IV] Title: 설탕 배달, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int answer = -1;
+		
+		for(int i = N / 5; i >= 0; i--) {
+			if((N - (5 * i)) % 3 == 0){
+				answer = i + ((N - (5 * i)) / 3);
+				break;
+			}
+		}
+		
+		System.out.println(answer);
+	}
+
+}
