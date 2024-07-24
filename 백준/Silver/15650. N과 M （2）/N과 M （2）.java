72616e275706f46fecfe3e452be6c54331a96d3a
GyeongHyang Jun <48426244+HappyHyang@users.noreply.github.com>
2024-07-24 23:39:38 +0900

    [Silver III] Title: N과 M (2), Time: 80 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static StringBuilder output = new StringBuilder();
+	static int[] selected;
+
+	static int N, M;
+
+	public static void main(String[] args) throws Exception {
+		initialize();
+		solution(1, 0);
+		System.out.print(output);
+	}
+
+	static void initialize() throws IOException {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer tokens = new StringTokenizer(input.readLine());
+		N = Integer.parseInt(tokens.nextToken());
+		M = Integer.parseInt(tokens.nextToken());
+		selected = new int[M];
+	}
+
+	static void solution(int start, int cnt) {
+		if (cnt == M) {
+			for (int i : selected) {
+				output.append(i).append(' ');
+			}
+			output.append('\n');
+			return;
+		}
+
+		for (int i = start; i <= N; i++) {
+			selected[cnt] = i;
+			solution(i + 1, cnt + 1);
+		}
+	}
+}
