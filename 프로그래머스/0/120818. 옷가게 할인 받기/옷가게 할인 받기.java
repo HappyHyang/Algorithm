925f870a5383514277689e956053f9d08f40a83c
GyeongHyang Jun <48426244+HappyHyang@users.noreply.github.com>
2024-11-19 10:23:48 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,13 +1,12 @@
 class Solution {
     public int solution(int price) {
-        double discount = 1;
         if (price >= 500_000) {
-            discount = 0.8;
+            return (int) (price * 0.8);
         } else if (price >= 300_000) {
-            discount = 0.9;
+            return (int) (price * 0.9);
         } else if (price >= 100_000) {
-            discount = 0.95;
+            return (int) (price * 0.95);
         }
-        return (int) (price * discount);
+        return price;
     }
 }
