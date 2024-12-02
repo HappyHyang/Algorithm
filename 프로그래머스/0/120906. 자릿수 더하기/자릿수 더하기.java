b753004231947213079829ca764a67f18d6b35f2
GyeongHyang Jun <48426244+HappyHyang@users.noreply.github.com>
2024-12-02 20:33:08 +0900

    [level 0] Title: 자릿수 더하기, Time: 1.99 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        return Arrays.stream(Integer.toString(n).split(""))
+                     .mapToInt(Integer::parseInt)
+                     .sum();
+    }
+}
