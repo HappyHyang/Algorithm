7eeaf072eb034402fa04069653e1ca78905af785
GyeongHyang Jun <48426244+HappyHyang@users.noreply.github.com>
2024-12-09 23:37:13 +0900

    [level 0] Title: 평행, Time: 0.04 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[][] dots) {
+        if (checkParallel(dots[0], dots[1], dots[2], dots[3])) return 1;
+        if (checkParallel(dots[0], dots[2], dots[1], dots[3])) return 1;
+        if (checkParallel(dots[0], dots[3], dots[1], dots[2])) return 1;
+        return 0;
+    }
+
+    private boolean checkParallel(int[] dot1, int[] dot2, int[] dot3, int[] dot4) {
+        return (dot2[1] - dot1[1]) * (dot4[0] - dot3[0]) == (dot4[1] - dot3[1]) * (dot2[0] - dot1[0]);
+    }
+}
