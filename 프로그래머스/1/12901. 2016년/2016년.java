99d6b4e0a2cfad8a74331fe8ef87ba99746658f9
GyeongHyang Jun <48426244+HappyHyang@users.noreply.github.com>
2024-12-13 23:52:40 +0900

    [level 1] Title: 2016년, Time: 1.50 ms, Memory: 86.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,9 @@
+import java.time.*;
+
+public class Solution {
+    public static String solution(int a, int b) {
+        LocalDate date = LocalDate.of(2016, a, b);
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        return dayOfWeek.toString().substring(0, 3);
+    }
+}
